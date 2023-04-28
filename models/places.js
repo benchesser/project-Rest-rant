06d59423d1605fbac5825c442535992396ee4bc6
Benjamin <ben.chesser19@gmail.com>
2023-04-27 20:15:23 -0700

    New places can be added and changed to node.js 12

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/H-Thai-ML-img.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/cat-cafe-img.jpg'
+}];
