46975c0a88424be0965462997ec9925c4bc03672
Benjamin <ben.chesser19@gmail.com>
2023-04-30 14:50:46 -0700

    fixed method override issue

--- controllers/places.js
+++ controllers/places.js
@@ -51,6 +51,32 @@ router.get('/:id/edit', (req, res) => {
     };
 });
 
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id);
+    if (isNaN(id)) {
+        res.render('error404');
+    } 
+    else if (!places[id]) {
+        res.render('error404');
+    }
+    else {
+        if (!req.body.pic) {
+            //Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        };
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        };
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        };
+
+
+        places[id] = req.body
+        res.redirect(`/places/${id}`);
+    };
+});
+
 //DELETE
 router.delete('/:id', (req, res) => {
     let id = Number(req.params.id);
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -17,7 +17,7 @@ function show (data) {
                 <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
                     Edit
                 </a>
-                <form method='POST' action={`/places${data.id}?_method=DELETE`}>
+                <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
                     <button type='submit' className='btn btn-danger'>
                         Delete
                     </button>
