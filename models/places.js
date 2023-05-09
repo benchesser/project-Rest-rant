a95a3cd117296c14e8aec94a53a51786116f46cb
Benjamin <ben.chesser19@gmail.com>
2023-05-08 19:41:58 -0700

    fixed mongoose routes

--- models/places.js
+++ models/places.js
@@ -1,13 +1,36 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/H-Thai-ML-img.jpg'
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cat-cafe-img.jpg'
-}];
+
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pic: String,
+    cuisines: { type: String, required:true },
+    city: { type: String, default: 'Anytown' },
+    state: { type: String, default: 'USA' },
+    founded: Number,
+});
+
+module.exports = mongoose.model('Place', placeSchema)
+
+
+
+
+
+
+
+
+
+
+// module.exports = [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '/images/H-Thai-ML-img.jpg'
+// }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/cat-cafe-img.jpg'
+// }];
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -10,7 +10,7 @@ function edit_form (data) {
                     <div className='row'>
                         <div className='form-group col-sm-6'>
                             <label htmlFor='name'>Place Name</label>
-                            <input className='form-control' id='name' name='name' value={data.place.name} required />
+                            <input className='form-control' id='name' name='name' defaultValue={data.place.name} required />
                         </div>
                         <div className='form-group col-sm-6'>
                             <label htmlFor='pic'>Place Picture</label>
