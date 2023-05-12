778a8156c242910eef3b03dad2745ba64a1c65c3
Benjamin <ben.chesser19@gmail.com>
2023-05-11 18:03:46 -0700

    add comments

--- models/index.js
+++ models/index.js
@@ -5,3 +5,4 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 mongoose.set('strictQuery', true);
 
 module.exports.Place = require('./places');
+module.exports.Comment = require('./comment');
