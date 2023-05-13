e9a73480c2fda290ba34ffc948e1b46e2c759699
Benjamin <ben.chesser19@gmail.com>
2023-05-12 21:27:34 -0700

    added comment form need to fix route still but it does post a comment on the page

--- controllers/places.js
+++ controllers/places.js
@@ -25,13 +25,15 @@ router.post('/', (req, res) => {
 
 //New
 router.get('/new', (req, res) => {
-    res.send('');
+    res.render('places/new');
 });
 
 //:id
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+    .populate('comments')
     .then(place => {
+        console.log(place.comments);
         res.render('places/show', { place });
     })
     .catch(err => {
@@ -52,6 +54,29 @@ router.delete('/:id', (req, res) => {
 //EDIT
 router.get('/:id/edit', (req, res) => {
     res.send('')
+});
+
+//Comment
+router.post('/:id/comment', (req, res) =>{
+    console.log(req.body);
+    req.body.rant = req.body.rant ? true : false
+    db.Place.findById(req.params.id)
+    .then(place => {
+        db.Comment.create(req.body)
+        .then(comment => {
+            place.comments.push(comment.id);
+            place.save()
+            .then(() => {
+                res.redirect(`/places${req.params.id}`);
+            })
+        })
+        .catch(err => {
+            res.render('error404');
+        });
+    })
+    .catch(err => {
+        res.render('error404');
+    });
 });
 
 //Rant
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react');
 const Def = require('../default');
 
 function show (data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className='border'>
+                    <h2 className='rant'>{c.rant ? 'Rant! 🤬' : 'Rave ! 😍'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            );
+        });
+    }
     return (
         <Def>
             <main>
@@ -26,11 +45,33 @@ function show (data) {
                         </h4>
                         
                         <h2>Comments</h2>
-                        <p>No comments yet!</p>
-                        <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
+                        {comments}
+                        <form method='POST' action={`/places/${data.place.id}/comment`}>
+                            <div className='row'>
+                                <div className='form-group col-sm-6'>
+                                    <label htmlFor='author'>Author</label>
+                                    <input className='form-control' id='author' name='author' />
+                                </div>
+                                <div className='form-group col-sm-6'>
+                                    <label htmlFor='content'>Content</label>
+                                    <input className='form-control' id='content' name='content' />
+                                </div>
+                                <div className='form-group col-sm-6'>
+                                    <label htmlFor="stars">Star Rating</label>
+                                    <input className='form-control' type='range' step='0.5' min='1' max='5' id='stars' name='stars' />
+                                </div>
+                                <div className='col-sm-2'>
+                                    <label htmlFor="rant">Rant?</label>
+                                    <input className='' type="checkbox" name="rant" id="rant" />
+                                </div>
+                            </div>
+                            <input type="submit" className='btn btn-primary' value='Add Comment' />
+                        </form>
+                        <hr/>
+                        <a href={`/places/${data.place.id}/edit`} className='btn btn-warning'>
                             Edit
                         </a>
-                        <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                        <form method='POST' action={`/places/${data.place.id}?_method=DELETE`}>
                             <button type='submit' className='btn btn-danger'>
                                 Delete
                             </button>
